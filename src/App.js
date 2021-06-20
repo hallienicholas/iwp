1b2d4ca09a37700792b7eef2484cbd53a76a2e93
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-06-20 10:35:19 -0400

    tests

--- src/App.js
+++ src/App.js
@@ -1,33 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 
-function App() {
-  return (
-    <View>
-    <Button
-      title="Say Hello"
-      onPress={() => {
-        setTimeout(() => {
-          setMessage("Hello Tester");
-        }, Math.floor(Math.random() * 200));
-      }}
-    />
-    {message && (
-      <Text style={styles.messageText} testID="printed-message">
-        {message}
-      </Text>
-    )}
-  </View>
-);
-};
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
 
-const styles = StyleSheet.create({
-messageText: {
-  fontFamily: "Arial",
-  fontSize: 38,
-  textAlign: "center",
-  marginTop: 10
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleRestart = () => {
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+            onRestart={this.handleRestart}
+          />
+        </main>
+      </div>
+    );
+  }
 }
-});
 
 export default App;
