3638cf91152b1684f103ef666f1fe6d8d8c1b6fe
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-06-20 10:31:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Text, TextInput, View, StyleSheet } from "react-native";
+import { Button, Text, TextInput, View, StyleSheet } from "react";
 
 const App = () => {
   const [message, setMessage] = React.useState();
