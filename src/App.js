3dfff4537fcb440d7b91c7060231fe57575a655e
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-06-20 11:17:43 -0400

    tests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo.svg" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
