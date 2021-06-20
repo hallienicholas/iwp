4f0928b46eac7bc7b445e545391d98a489c6019b
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-06-20 10:28:17 -0400

    more testing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Text, TextInput, View, StyleSheet } from "react-native";
 
-function App() {
+const App = () => {
+  const [message, setMessage] = React.useState();
+
   return (
     <View>
-    <Button
-      title="Say Hello"
-      onPress={() => {
-        setTimeout(() => {
-          setMessage("Hello Tester");
-        }, Math.floor(Math.random() * 200));
-      }}
-    />
-    {message && (
-      <Text style={styles.messageText} testID="printed-message">
-        {message}
-      </Text>
-    )}
-  </View>
-);
+      <Button
+        title="Say Hello"
+        onPress={() => {
+          setTimeout(() => {
+            setMessage("Hello Tester");
+          }, Math.floor(Math.random() * 200));
+        }}
+      />
+      {message && (
+        <Text style={styles.messageText} testID="printed-message">
+          {message}
+        </Text>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-messageText: {
-  fontFamily: "Arial",
-  fontSize: 38,
-  textAlign: "center",
-  marginTop: 10
-}
+  messageText: {
+    fontFamily: "Arial",
+    fontSize: 38,
+    textAlign: "center",
+    marginTop: 10
+  }
 });
 
 export default App;
