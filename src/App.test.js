2abf60aab75d7a2d47c8d736aaab655430f35217
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-07-19 22:12:39 -0400

    testing heroku/circleci

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import Enzyme, { mount } from 'enzyme';
 import App from './App';
+import Quotes from './components/Quotes';
 
-test('renders learn react link', () => {
+// Add your adapter version below
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('displays a quote', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  const quote = document.querySelector('#text p');
+  expect(quote).toBeInTheDocument();
+  expect(quote).not.toBeEmptyDOMElement();
+});
+
+it('calls generateRandomQuote prop function when next button is clicked', () => {
+  const generateRandomQuoteFn = jest.fn();
+  const quote = mount(
+    <Quotes generateRandomQuote={generateRandomQuoteFn} quote={{}} />
+  );
+  const generateBtn = quote.find('#new-quote');
+
+  generateBtn.simulate('click');
+  expect(generateRandomQuoteFn).toHaveBeenCalledTimes(1);
 });
