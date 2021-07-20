319214f647d7430752b89bc542c169eadd25c805
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-07-19 22:19:58 -0400

    changes to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,6 @@
-import { render } from '@testing-library/react';
-import Enzyme, { mount } from 'enzyme';
-import App from './App';
-import Quotes from './components/Quotes';
+import sum from './sum';
 
-// Add your adapter version below
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-test('displays a quote', () => {
-  render(<App />);
-  const quote = document.querySelector('#text p');
-  expect(quote).toBeInTheDocument();
-  expect(quote).not.toBeEmptyDOMElement();
-});
-
-it('calls generateRandomQuote prop function when next button is clicked', () => {
-  const generateRandomQuoteFn = jest.fn();
-  const quote = mount(
-    <Quotes generateRandomQuote={generateRandomQuoteFn} quote={{}} />
-  );
-  const generateBtn = quote.find('#new-quote');
-
-  generateBtn.simulate('click');
-  expect(generateRandomQuoteFn).toHaveBeenCalledTimes(1);
+it('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
 });
