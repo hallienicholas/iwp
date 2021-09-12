95af1770fc79b5d58adcb7a63d872696248b13ae
isaacparada <79061106+isaacparada@users.noreply.github.com>
2021-09-12 15:22:58 -0400

    moving many files.. and whatnot

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
+import { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App() {
+  const [pumpList, setPumpList] = useState([]);
+
+  const getData = () => {
+    Axios.get("http://localhost:3001/data").then((response) => {
+      setPumpList(response.data);
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>IWP Database</h1>
+      <div className = "get_data_button">
+        <button onClick={getData}>
+          Show Data</button>  
+
+        {pumpList.map((val, key) => {
+          return (
+            <div className = "dataTables">
+              <h3>data transmission id: {val.iwp_sensor_data_id}</h3>
+              <h3>pump id: {val.iwp_pump_id_fk}</h3>
+              <h3>battery percentage: {val.battery_percentage}</h3> 
+            </div>
+          )
+        })}
+
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
