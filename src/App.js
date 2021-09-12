1c20939d41c95fa5de0083fc6ca251fd2a8c9abb
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-09-12 14:09:54 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Axios from 'axios'
-import { useState } from "react";
 
 class App extends Component {
   render() {
