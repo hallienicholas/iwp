9d914ad11e9fc0a99b19e606f926d68718ced607
isaacparada <79061106+isaacparada@users.noreply.github.com>
2021-09-15 15:58:26 -0400

    functionality for multiple pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import './App.css';
-import Axios from 'axios'
-import { useState } from "react";
 
-function App() {
-  const [pumpList, setPumpList] = useState([]);
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-  const getData = () => {
-    Axios.get("http://localhost:3001/data").then((response) => {
-      setPumpList(response.data);
-    });
-  };
+import DbPage from "./pages";
 
-  return (
-    <div className="App">
-      <h1>IWP Database</h1>
-      <div className = "get_data_button">
-        <button onClick={getData}>
-          Show Data</button>  
-
-        {pumpList.map((val, key) => {
-          return (
-            <div className = "dataTables">
-              <h3>data transmission id: {val.iwp_sensor_data_id}</h3>
-              <h3>pump id: {val.iwp_pump_id_fk}</h3>
-              <h3>battery percentage: {val.battery_percentage}</h3> 
-            </div>
-          )
-        })}
-
-      </div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+       <Route exact path="/" component={DbPage} />
+      </Router>
+    );
+  }
 }
 
 export default App;
