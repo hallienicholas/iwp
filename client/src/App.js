418d320c65f0acd6a91b5336bb5b62f7848f3b65
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-10-02 13:26:33 -0400

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import Sidebar from './Sidebar';
-import Navbar from './Navbar';
-import Dashboard from './Dashboard';
-import LoginRibbon from './LoginRibbon';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import Dashboard from "./Dashboard";
+import LoginRibbon from "./LoginRibbon";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+  //Link,
+  //Redirect
+} from "react-router-dom";
+import DbPage from "./pages";
+import LoginPage from "./pages/login";
 // 
 class App extends Component {
   render() {
     return (
       <div className="App" id="wrapper">
-        <Sidebar />
+        <Router>
+          <Sidebar />
+            <Switch>
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/" component={DbPage} />
+            </Switch>
         <div id="content-wrapper" class="d-flex flex-column">
+         
           <Navbar />
-          <div class="container-fluid">
+          <div className="container-fluid">
             <Dashboard />
           </div>
         </div>
+        </Router> 
       </div>
     );
   }
