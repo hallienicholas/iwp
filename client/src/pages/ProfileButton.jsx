596ee6fbae2168fb6933275a5d99d82e2ad34556
adamhungerford <79060679+adamhungerford@users.noreply.github.com>
2021-10-12 08:46:32 -0400

    Create ProfileButton.jsx

---
+++ client/src/pages/ProfileButton.jsx
@@ -0,0 +1,9 @@
+class ProfileButton extends Component(){
+    render(){
+        return(
+            <a className="ml-1 mr-4" href="/"><i className="fas fa-user fa-2x"></i></a>
+        );
+    }
+}
+
+export default ProfileButton;
