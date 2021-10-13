ccb844f7aedcb12ab49c2528d68b50625e3a08dc
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-10-13 18:27:51 -0400

    profile button and registration page

--- client/src/pages/ProfileButton.jsx
+++ client/src/pages/ProfileButton.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class ProfileButton extends Component{
     render(){
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Axios from 'axios'
 import { useState } from "react";
 import { Link } from "react-router-dom";
