ba4a9df0a135cfc1be6e16241a50a54d913042e4
hallienicholas <57117680+hallienicholas@users.noreply.github.com>
2021-11-11 18:28:01 -0500

    Revert "Merge pull request #35 from hallienicholas/hallie-registration"
    This reverts commit 66e517ed5caf5c16d6800613937104f31bb799a6, reversing
    changes made to 032dcb269482aa02f9ba28d1fac5ae28a8087d81.

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,86 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import Axios from 'axios'
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import LoginRibbon from "../LoginRibbon";
 
-function Registration () {
-
-    /* Registration States */
-    const [firstNameReg, setFirstNameReg] = useState("");
-    const [lastNameReg, setLastNameReg] = useState("");
-    const [usernameReg, setUsernameReg] = useState("");
-    const [passwordReg, setPasswordReg] = useState("");
-    const [loginStatus, setLoginStatus] = useState("");
-
-    const register = () => {
-      Axios.post("http://localhost:3001/register", {
-        username: usernameReg, 
-        password: passwordReg,
-        firstname: firstNameReg,
-        lastname: lastNameReg
-      }).then((response) => {
-        console.log(response);
-      });
-    };
-
+class Registration extends Component{
+    render(){
         return(
             <div id = "wrapper">
                 <LoginRibbon />
                 <div className = "mt-auto mb-auto mr-auto ml-auto">
                 
-                {/* Beginning of registration Section */}
-                    Registration
-                    <div>
-                        <input 
-                        className="mt-2"
-                        type="text"
-                        placeholder="First Name"
-                        onChange={(event) => {
-                            setFirstNameReg(event.target.value); 
-                            }}
-                        />
-                        <br />
-                        <input 
-                        className="mt-2"
-                        type="text"
-                        placeholder="Last Name"
-                        onChange={(event) => {
-                            setLastNameReg(event.target.value); 
-                            }}
-                        />
-                        <br />
-                        <input 
-                        className="mt-2"
-                        type="text" 
-                        name="username"
-                        placeholder="Email Address"
-                        onChange={(event) => {
-                            setUsernameReg(event.target.value); 
-                            }}
-                        />
-                        <br />
-                        <input 
-                        className="mt-2"
-                        type="text"
-                        placeholder="Password"
-                        onChange={(event) => {
-                            setPasswordReg(event.target.value); 
-                            }}
-                        />
-                        <br />
-                        <br />
-                    <div className="btn-group mt-2 mb-2" role="group">
-                    <button className="btn btn-primary shadow" onClick={register}>Submit</button>
-
-                    <h1>{loginStatus}</h1>
-                    <p>Already have an account? <Link to="/login" className="link">Login</Link></p>
-                    </div>
-                </div>
-                    {/* End of Create Account Section */}
+                <input 
+                    className="mt-2"
+                    type="text" 
+                    name="username"
+                    placeholder="Username"
+                    /> <br />
+                <input 
+                    className="mt-2"
+                    type="text"
+                    placeholder="Password"             
+                /> <br />
+                <input 
+                    className="mt-2"
+                    type="text"
+                    placeholder="Confirm password"
+                />
+                <br />
+                <p>Already have an account? <Link to="/login" className="link">Login</Link></p>
+                <button className="btn btn-primary">Submit</button>
                 </div>
             </div>
         );
+    }
 }
 
 export default Registration;
--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use('/loginpop', (req, res) => {
     });
   });
 
-app.post('/register', (req, res) => {
+app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     
