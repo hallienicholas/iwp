37efae2a19cd9a357fc8c8778b1263bf25132672
adamhungerford <79060679+adamhungerford@users.noreply.github.com>
2021-11-15 20:35:39 -0500

    Fixing disabled About Us bug.
    About Us navbar link used "to" attr. instead of "href" to link

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -7,7 +7,7 @@ class Navbar extends Component {
 		return(
 			<nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 				<a className="mr-3 nav-item nav-link text-uppercase" href="/">Home</a>
-				<a className="mr-3 nav-item nav-link text-uppercase" to="/about">About Us</a>
+				<a className="mr-3 nav-item nav-link text-uppercase" href="/about">About Us</a>
 				<a className="mr-3 nav-item nav-link text-uppercase" href="/contact">Contact</a>
 				<a className="mr-3 nav-item nav-link text-uppercase" href="http://iwp.cs.messiah.edu/about/faq.php">FAQ</a>
 				<MessagesDropDown />
