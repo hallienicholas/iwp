e5000d744a8a56b16aaf7a61ab1b9c63d9e698f6
adamhungerford <79060679+adamhungerford@users.noreply.github.com>
2022-01-15 14:40:14 -0500

    Fixing more issues with vert alignment
    Put some elements in a column to fix vert align

--- client/src/login/ForgotPassword.jsx
+++ client/src/login/ForgotPassword.jsx
@@ -7,17 +7,23 @@ class ForgotPassword extends Component{
         return(
             <div id="wrapper">
                 <LoginRibbon />
-                <div className = "mb-auto mr-auto ml-auto mt-auto">
-                    Reset Password<br />
-                    <input 
-                        className="mt-2"
-                        type="text" 
-                        name="email"
-                        placeholder="Email"
-                    />
-                    <br />
-                    <button className="btn btn-primary mt-2 mb-2">Send Password Reset</button>
-                    <p>I remembered my password. <Link to="/login" className="link">Log me in</Link></p>
+                <div className="container mb-auto mt-auto">
+                    <div className="row">
+                        <div className="col-sm"></div>
+                        <div className="col-sm">
+                            Reset Password<br />
+                            <input 
+                                className="mt-2"
+                                type="text" 
+                                name="email"
+                                placeholder="Email"
+                            />
+                            <br />
+                            <button className="btn btn-primary mt-2 mb-2">Send Password Reset</button>
+                            <p>I remembered my password. <br></br><Link to="/login" className="link">Log me in</Link></p>
+                        </div>
+                        <div className="col-sm"></div>
+                    </div>
                 </div>
             </div>
         )
